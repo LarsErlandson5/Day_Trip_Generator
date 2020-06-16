243001d93d0029ae4aeae2441061d874029dc0ac
unknown <larserlandson5@gmail.com>
2020-06-15 22:02:38 -0500

    Created functino to generate a random number

--- app.js
+++ app.js
@@ -3,5 +3,14 @@
 let destinationLocations = ['Florida','Texas','Wisconsin','New York','Califorina'];
 let placesToEat = ['Seafood','Steak House', 'Brewery', 'Italian', 'Food Truck', 'Chinese'];
 let modeOfTravel = ['Plane','Train','Automobile']
+let random = 6;
 
-console.log(destinationLocations[0]);
+//build a function to generate a whole random number.
+
+function randomNumberGenerator(highValue){
+    return Math.floor((Math.random() * Math.floor(highValue))+1)
+}
+
+for(let i =0; i<100;i++){
+console.log(randomNumberGenerator(4));
+}
