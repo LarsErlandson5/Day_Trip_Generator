2d802d8c8df3282e00ff40ef8a456cb4ef0bd6ec
unknown <larserlandson5@gmail.com>
2020-06-16 18:05:33 -0500

    Adding array formOfEntertainment and including it in console.log

--- app.js
+++ app.js
@@ -3,24 +3,28 @@
 let destinationLocations = ['Florida', 'Texas', 'Wisconsin', 'New York', 'Califorina'];
 let finalDestination = ''
 let placesToEat = ['Seafood', 'Steak House', 'Brewery', 'Italian', 'Food Truck', 'Chinese'];
-let finalPlaceToEat = "";
+let finalPlaceToEat = '';
 let modeOfTravel = ['Plane', 'Train', 'Automobile', 'Boat'];
-let finalModeOfTravel = "";
-let result = 0;
+let finalModeOfTravel = [''];
+let formOfEnterTainment = ['Concert', 'Pool', 'Beach','site seeing'];
+let finalFormOfEntertainment = ''
+let answer = 0;
 
 //build a function to generate a whole random number.
 
 function randomNumberGenerator(highValue) {
-    result = Math.floor((Math.random() * highValue)+1)-1
-    console.log(result);
+    answer = Math.floor((Math.random() * highValue)+1)-1; 
+    console.log(answer);
 }
-
+// call out array positions to determine trip
 randomNumberGenerator(destinationLocations.length);
-finalDestination = destinationLocations[result]
+finalDestination = destinationLocations[answer]
 randomNumberGenerator(placesToEat.length);
-finalPlaceToEat = placesToEat[result];
+finalPlaceToEat = placesToEat[answer];
 randomNumberGenerator(modeOfTravel.length);
-finalModeOfTravel = modeOfTravel[result];
+finalModeOfTravel = modeOfTravel[answer];
+randomNumberGenerator(formOfEnterTainment.length);
+finalFormOfEntertainment = formOfEnterTainment[answer];
 
+console.log(finalDestination, finalPlaceToEat, finalModeOfTravel, finalFormOfEntertainment);
 
-console.log(finalDestination, finalPlaceToEat, finalModeOfTravel);
