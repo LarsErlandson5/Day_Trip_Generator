ec333a00e8d53a55eb27e22bfade278a708a832c
unknown <larserlandson5@gmail.com>
2020-06-15 21:08:37 -0500

    Adding defined array variables for trip locations, food destinations and modes of travel.

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict"
+//user needs destinations, restaurants, mode of transportation, entertainment
+let destinationLocations = ['Florida','Texas','Wisconsin','New York','Califorina'];
+let placesToEat = ['Seafood','Steak House', 'Brewery', 'Italian', 'Food Truck', 'Chinese'];
+let modeOfTravel = ['Plane','Train','Automobile']
+
+console.log(destinationLocations[0]);
