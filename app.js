2b8ccf8bee43bd752facdb489324885c02785809
unknown <larserlandson5@gmail.com>
2020-06-16 19:47:57 -0500

    Added getValueFromArray function

--- app.js
+++ app.js
@@ -6,36 +6,46 @@ let placesToEat = ['Seafood', 'Steak House', 'Brewery', 'Italian', 'Food Truck',
 let finalPlaceToEat = '';
 let modeOfTravel = ['Plane', 'Train', 'Automobile', 'Boat'];
 let finalModeOfTravel = [''];
-let formOfEnterTainment = ['Concert', 'Pool', 'Beach','Monument'];
+let formOfEnterTainment = ['Concert', 'Pool', 'Beach', 'Monument'];
 let finalFormOfEntertainment = ''
 let answer = 0;
 
 //build a function to generate a whole random number.
 
 function randomNumberGenerator(highValue) {
-    answer = Math.floor((Math.random() * highValue)+1)-1; 
-    console.log(answer);
+    let randomNumber = Math.floor((Math.random() * highValue) + 1) - 1;
+    console.log(randomNumber);
+    return randomNumber;
+}
+
+function getValueFromArray(array, randomNumber) {
+    let result = array[randomNumber];
+    return result;
 }
 // call out array positions to determine trip
-randomNumberGenerator(destinationLocations.length);
-finalDestination = destinationLocations[answer]
-randomNumberGenerator(placesToEat.length);
-finalPlaceToEat = placesToEat[answer];
-randomNumberGenerator(modeOfTravel.length);
-finalModeOfTravel = modeOfTravel[answer];
-randomNumberGenerator(formOfEnterTainment.length);
-finalFormOfEntertainment = formOfEnterTainment[answer];
+let = getValueFromArray(destinationLocations, randomNumberGenerator(destinationLocations.length));
 
-console.log("Here is a randomly generated trip to",finalDestination, "eating", finalPlaceToEat, "food. You'll take a", finalModeOfTravel,"to get there and head to a", finalFormOfEntertainment),"for entertainment!";
 
-for(let i=0; i<10; i++){
-let decision = prompt("Enter Yes to confirm trip,");
-    if(decision == "Yes" ){
-        console.log("yes was typed")
-        break; 
-    }
-    else{
-        console.log("something else was typed...")
-        break;
-    }
-}
+
+
+// finalDestination = destinationLocations[answer]
+// randomNumberGenerator(placesToEat.length);
+// finalPlaceToEat = placesToEat[answer];
+// randomNumberGenerator(modeOfTravel.length);
+// finalModeOfTravel = modeOfTravel[answer];
+// randomNumberGenerator(formOfEnterTainment.length);
+// finalFormOfEntertainment = formOfEnterTainment[answer];
+
+// console.log("Here is a randomly generated trip to",finalDestination, "eating", finalPlaceToEat, "food. You'll take a", finalModeOfTravel,"to get there and head to a", finalFormOfEntertainment),"for entertainment!";
+
+// for(let i=0; i<10; i++){
+// let decision = prompt("Enter Yes to confirm trip,");
+//     if(decision == "Yes" ){
+//         console.log("Yes was typed")
+//         break; 
+//     }
+//     else{
+//         console.log("something else was typed...")
+//         break;
+//     }
+// }
