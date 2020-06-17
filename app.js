795e3c30249feee4f2f94eb580539ff5a4948166
unknown <larserlandson5@gmail.com>
2020-06-16 22:18:56 -0500

    Creating a branch

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 "use strict"
 //user needs destinations, restaurants, mode of transportation, entertainment
 let destinationLocations = ['Florida', 'Texas', 'Wisconsin', 'New York', 'Califorina'];
-let placesToEat = ['Seafood', 'Steak House', 'Brewery', 'Italian', 'Food Truck', 'Chinese'];
+let placesToEat = ['Seafood', 'Steak House', 'Brewery', 'Italian', 'Food Truck', 'Chinese','BBQ'];
 let modeOfTravel = ['Plane', 'Train', 'Automobile', 'Boat'];
 let formOfEnterTainment = ['Concert', 'Pool', 'Beach', 'Monument', 'Football Game'];
+let bigArray = [destinationLocations[1],placesToEat[6],modeOfTravel[0],formOfEnterTainment[4]];
+
+//console.log(bigArray);
 
 function randomNumberGenerator(highValue) {
     let randomNumber = Math.floor((Math.random() * highValue) + 1) - 1;
@@ -15,6 +18,14 @@ function getValueFromArray(array, randomNumber) {
     let result = array[randomNumber];
     return result;
 }
+function getValueFromLargerArray(array, randomNumber){
+    let result = array[randomNumber];
+}
+
+
+
+let totalStuff = getValueFromArray(bigArray,randomNumberGenerator(bigArray.length));
+console.log(totalStuff);
 
 let destination = getValueFromArray(destinationLocations, randomNumberGenerator(destinationLocations.length));
 let eatAt = getValueFromArray(placesToEat, randomNumberGenerator(placesToEat.length));
@@ -27,36 +38,8 @@ if (prompt("Type yes to confim trip") == "yes") {
     console.log("You are going to",destination,"and eating at",eatAt,". You're taking a",travel,"and will be going to",entertainment,"for fun!")
 
 }
-else { 
-    destination = getValueFromArray(destinationLocations, randomNumberGenerator(destinationLocations.length));
-    eatAt = getValueFromArray(placesToEat, randomNumberGenerator(placesToEat.length));
-    travel = getValueFromArray(modeOfTravel, randomNumberGenerator(modeOfTravel.length));
-    entertainment = getValueFromArray(formOfEnterTainment, randomNumberGenerator(formOfEnterTainment.length));
-    console.log("Instead!!! You're going to",destination,"and eating",eatAt,". You're taking a",travel,"and will be going to",entertainment,"for fun!")
-
+else { prompt("Type the part of your trip would you like to edit"./n "destination;eat;travel,entertainment")
+    
 }
 
 
-
-
-// finalDestination = destinationLocations[answer]
-// randomNumberGenerator(placesToEat.length);
-// finalPlaceToEat = placesToEat[answer];
-// randomNumberGenerator(modeOfTravel.length);
-// finalModeOfTravel = modeOfTravel[answer];
-// randomNumberGenerator(formOfEnterTainment.length);
-// finalFormOfEntertainment = formOfEnterTainment[answer];
-
-// console.log("Here is a randomly generated trip to",finalDestination, "eating", finalPlaceToEat, "food. You'll take a", finalModeOfTravel,"to get there and head to a", finalFormOfEntertainment),"for entertainment!";
-
-// for(let i=0; i<10; i++){
-// let decision = prompt("Enter Yes to confirm trip,");
-//     if(decision == "Yes" ){
-//         console.log("Yes was typed")
-//         break; 
-//     }
-//     else{
-//         console.log("something else was typed...")
-//         break;
-//     }
-// }
